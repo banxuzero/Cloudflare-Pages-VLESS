4cf8e9e1ff79474540a4ca126b7d463e251ed4c5
banxuzero <banxuzero@163.com>
2024-10-09 22:58:58 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -3,7 +3,7 @@
 import { connect } from 'cloudflare:sockets';
 
 // 建议修改为自己的 UUID
-let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
+let userID = '1e38c932-4b44-4c5d-a23b-4f244cea1bd3';
 
 // 生成配置文件的 CF 优选 IP
 const bestCFIP = "www.gov.se"
